fc8a32b4294b37c301768e57e675f58cec971b0a
Claude Famador <cmfamador@student.apc.edu.ph>
2025-09-22 21:11:16 +0800

    file pathing fix

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabaseClient";
 import { useState, useEffect } from "react";
 
 export default function LoginPage() {
