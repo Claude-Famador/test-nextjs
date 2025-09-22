b7df6367b2fc7994be3de31c973ce5eaa5559d39
Claude Famador <cmfamador@student.apc.edu.ph>
2025-09-22 15:38:53 +0800

    commit changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,7 @@
-import LoginPage from "./login-page/page";
-
 export default function Home() {
   return (
     <div>
 
     </div>
   );
-}
-
-export default function Login() {
-  return <LoginPage />;
 }
